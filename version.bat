1550c7a5dbbc4189a22435812cb3c24888a29711
Ilay Atia <ilaywork06@gmail.com>
2026-03-28 21:07:22 +0300

    refactor: Remove backup and restore logic for community.json in version.bat

--- version.bat
+++ version.bat
@@ -1,22 +1,3 @@
 @echo off
-echo Updating FundCompare...
-
-cd /d "%~dp0"
-
-:: Save community.json before pull
-if exist community.json (
-    copy community.json community_backup.json
-)
-
-:: Discard all local changes and pull
-git checkout .
 git pull
-
-:: Restore community.json
-if exist community_backup.json (
-    copy community_backup.json community.json
-    del community_backup.json
-)
-
-echo Update complete!
 pause
